06915ac9cc18176f0486c4d396d0016a91399196
TamerlanAlliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-10-16 00:39:54 -0700

    Telebe

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Student student = new Student();
+
+student.Name;
+student.SurName;
+student.Group;
+student.Point;
+student.IsGraduated;
+
+student.ShowInfo()
---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿public class Student
+{
+    public string Name;
+    public string SurName;
+    public string Group;
+    public string Point;
+    public bool IsGraduated;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine(Name,SurName,Group,Point,IsGraduated);
+    }
+}
+
