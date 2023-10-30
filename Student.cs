13a522e2d0c59c67dd1713e7a71a684db55cf129
TamerlanAlliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-10-30 09:19:00 -0700

    Student.cs

--- Student.cs
+++ Student.cs
@@ -1,14 +1,44 @@
-﻿public class Student
+public class Student
 {
     public string Name;
-    public string SurName;
+    public string Surname;
     public string Group;
-    public string Point;
-    public bool IsGraduated;
+    public int Point;
+    public bool isGraduated;
 
-    public void ShowInfo()
+    public Student(string name, string surname, string group, int point, bool isgraduated)
     {
-        Console.WriteLine(Name,SurName,Group,Point,IsGraduated);
+        this.Name = name;
+        this.Surname = surname;
+        this.Group = group;
+        this.Point = point;
+        this.isGraduated = isgraduated;
+    }
+
+    public void FullName()
+    {
+        
+        Console.WriteLine($"Name : {Name} , Surname : {Surname}");
+    }
+
+    public void FullInfo()
+    {
+        Console.WriteLine($"Name : {Name} , Surname : {Surname} , Group : {Group} , Point : {Point} , isGraduated : {isGraduated}");
+        if (this.isGraduated==true)
+        {
+            Console.WriteLine("Mezun olub");
+        }
+        else
+        {
+            Console.WriteLine("Mezun olmayib");
+            if (this.Point > 80)
+            {
+                Console.WriteLine("Bal yeterlidir Imtahana gire biler");
+            }
+            else
+            {
+                Console.WriteLine("Bal yetersizdir Kesildi");
+            }
+        }        
     }
 }
-
